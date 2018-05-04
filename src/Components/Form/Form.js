eb6abfd4de6a51ec5e2fde8bcc7f663e684ded30
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-05-04 11:06:36 -0600

    through Part 1 Step1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
 
 export default class Form extends Component{
+    constructor(){
+        super()
+        this.state={
+            name:'',
+            price:0,
+            imgurl:''
+        }
+        this.handleChangeName= this.handleChangeName.bind(this)
+        this.handleChangePrice= this.handleChangePrice.bind(this)
+        this.handleChangeImg=this.handleChangeImg.bind(this)
+        this.postProduct=this.postProduct.bind(this)
+        this.clearInputBoxes=this.clearInputBoxes.bind(this)
+    }
+
+    handleChangeName(e) {
+        this.setState({name: e.target.value})
+    }
+
+    handleChangePrice(e){
+        this.setState({price: e.target.value})
+    }
+
+    handleChangeImg(e){
+        this.setState({imgurl: e.target.value})
+    }
+
+    postProduct(){}
+
+    clearInputBoxes(e){
+        this.setState({
+            name:'',
+            price: 0,
+            imgurl:''
+        })
+    }
+
 
     render(){
         return(
             <div>
-                Form
+                <p>Image URL: <input onChange= {this.handleChangeImg} /></p>
+                <p>Product Name: <input onChange={this.handleChangeName} /></p>
+                <p>Price: <input onChange={this.handleChangePrice}/></p>
+                <button onClick= {this.clearInputBoxes}>Cancel</button>
+                <button onClick={this.postProduct} >Add to Inventory</button>
             </div>
         )
     }
